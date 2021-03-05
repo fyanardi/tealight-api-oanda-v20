0d8d4bbb9e0cd54e5517a3ebbaffb63dfb0e6a17
Fredy Yanardi <fyanardi@gmail.com>
2021-03-05 23:39:14 +0800

    Private methods do not need to be synchronized + Javadocs

--- src/main/java/io/tealight/api/oanda/v20/endpoint/PricingEndpoints.java
+++ src/main/java/io/tealight/api/oanda/v20/endpoint/PricingEndpoints.java
@@ -39,6 +39,20 @@ public class PricingEndpoints {
         this.fxTradeContext = fxTradeContext;
     }
 
+    /**
+     * Subscribes to the price stream endpoint by account id. If there is an active price
+     * subscription for the account id, it will be cancelled first before re-subscribing with the
+     * additional instruments specified in the subscription map. The subscription map is a map of
+     * instrument to a callback. There is no restriction that every instrument must have a unique
+     * callback, hence one callback object can be used for multiple instruments.
+     *
+     * This will not affect any existing subscriptions.
+     *
+     * @param accountId the account id for the price subscription
+     * @param subscription a map of instrument to price stream callback
+     * @throws FxTradeException thrown if the request to the fxTrade server is not successful
+     * @throws IOException thrown if I/O Exception occurs during the subscription request
+     */
     public synchronized void subscribePriceStream(String accountId,
             Map<String, PricingStreamCallback> subscription) throws FxTradeException, IOException {
         SubscriptionInfo subscriptionInfo = subscriptionMap.get(accountId);
@@ -68,6 +82,17 @@ public class PricingEndpoints {
         subscribeStream(accountId, subscriptionInfo);
     }
 
+    /**
+     * Unsubscribes from a price stream endpoint by account id. This may or may not affect the
+     * underlying price subscription to the stream endpoint. If unsubscription of the specified
+     * callback causes no more callbacks that listen for the instrument, then the current price
+     * stream will be cancelled and a new one will be created.
+     *
+     * @param accountId the account id to unsubscribe from the price stream
+     * @param callback price stream callback to be unsubscribed
+     * @throws FxTradeException thrown if the request to the fxTrade server is not successful
+     * @throws IOException thrown if I/O Exception occurs during the subscription request
+     */
     public synchronized boolean unsubscribePriceStream(String accountId,
             PricingStreamCallback callback) throws FxTradeException, IOException {
         SubscriptionInfo subscriptionInfo = subscriptionMap.get(accountId);
@@ -111,7 +136,7 @@ public class PricingEndpoints {
         return true;
     }
 
-    private synchronized void subscribeStream(String accountId, SubscriptionInfo subscriptionInfo)
+    private void subscribeStream(String accountId, SubscriptionInfo subscriptionInfo)
             throws FxTradeException, IOException {
         Set<String> instrumentSet = subscriptionInfo.subscriptions.keySet();
         String[] instruments = instrumentSet.toArray(new String[instrumentSet.size()]);
@@ -138,7 +163,7 @@ public class PricingEndpoints {
         }
     }
 
-    private synchronized boolean unsubscribeStream(SubscriptionInfo subscriptionInfo) {
+    private boolean unsubscribeStream(SubscriptionInfo subscriptionInfo) {
         if (subscriptionInfo.streamSubscriber != null) {
             subscriptionInfo.streamSubscriber.cancel();
             subscriptionInfo.streamSubscriber = null;
